18ed3f0727a88efd011f9937c46ab94c073aa194
Rhilip <admin@rhilip.info>
2021-11-20 17:07:29 +0800

    init: create bug view

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import {h, ref, watch} from "vue";
+import {NAutoComplete, NTag, NHr} from "naive-ui";
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+const valueRef1 = ref('')
+const optionsRef1 = ref([])
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+watch(valueRef1, () => {
+  setTimeout(() => {   // 模拟异步加载
+    optionsRef1.value = [1,2,3].map((v) => {
+      return {
+        label: () => h(NTag, null, {default: () => valueRef1.value + v}),
+        value: valueRef1.value + v
+      }
+    })
+  }, 1e3)
+})
+
+const valueRef2 = ref('')
+const optionsRef2 = ref([])
+watch(valueRef2, () => {
+  setTimeout(() => {
+    optionsRef2.value = [1,2,3].map((v) => {
+      return {
+        label: valueRef2.value + v,
+        render: () => h(NTag, null, {default: () => valueRef2.value + v}),
+        value: valueRef2.value + v
+      }
+    })
+  }, 1e3)
+})
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div style="width: 480px; margin: 10px">
+    AutoComplete Which options label is NTag
+    <n-auto-complete :options="optionsRef1" v-model:value="valueRef1">
+    </n-auto-complete>
+
+    <n-hr />
+
+    AutoComplete Which options use render is NTag
+    <n-auto-complete :options="optionsRef2" v-model:value="valueRef2">
+    </n-auto-complete>
+  </div>
+</template>
