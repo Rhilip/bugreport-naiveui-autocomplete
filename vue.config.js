faa4237f71a7c04ee5a7557a85a5b407bad24555
Rhilip <admin@rhilip.info>
2021-11-20 17:25:17 +0800

    fix build

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/bugreport-naiveui-autocomplete/'
+    : '/',
   outputDir: 'docs'
 }
